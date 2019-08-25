81a072ff09db0b2e8a0b31917e302598eda3c01b
Damian Sandhu-Franceschi <damiansandhu-franceschi@Damians-MacBook-Pro.local>
2019-08-25 19:23:50 -0400

    adding skip button

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,27 @@
 <template>
   <div id="app">
-    <Portfolio prompt="DamianSandhu-Franceschi@Developer:~$ "/>
+    <Portfolio v-if="!skipAnimation" prompt="DamianSandhu-Franceschi@Developer:~$ " />
+    <PortfolioNoAnimation v-if="skipAnimation" prompt="DamianSandhu-Franceschi@Developer:~$ " />
+    <div class="skip-text" v-on:click="skipAnimation = true" v-if="!skipAnimation">Skip Animation</div>
   </div>
 </template>
 
 <script>
-import Portfolio from './components/Portfolio.vue'
+import Portfolio from "./components/Portfolio.vue";
+import PortfolioNoAnimation from "./components/PortfolioNoAnimation.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    Portfolio
+    Portfolio,
+    PortfolioNoAnimation
+  },
+  data: function() {
+    return {
+      skipAnimation: false
+    };
   }
-}
+};
 </script>
 
 <style>
@@ -21,8 +30,15 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #888888;
-  margin-left:auto;
-	margin-right:auto;
-  font-size:14px;
+  margin-left: auto;
+  margin-right: auto;
+  font-size: 14px;
+}
+.skip-text {
+  color: #00bbcc;
+  cursor: pointer;
+  position: absolute;
+  bottom: 0;
+  right: 0;
 }
 </style>
